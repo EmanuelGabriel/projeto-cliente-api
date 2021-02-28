dc483f901aa7f7a3740af4170fec15051b9fcf05
Samuel Gabriel Sousa <emanuel.gabriel.sousa@protonmail.com>
2021-02-28 14:37:59 -0300

    realizando o mapeamento da entidade de Cliente

--- src/main/java/br/com/emanuelgabriel/clienteapi/persistence/entity/Cliente.java
+++ src/main/java/br/com/emanuelgabriel/clienteapi/persistence/entity/Cliente.java
@@ -1,29 +1,46 @@
 package br.com.emanuelgabriel.clienteapi.persistence.entity;
 
 import javax.persistence.*;
+import java.time.LocalDate;
+import java.util.Objects;
+import java.util.function.Function;
 
 @Entity
 @Table(name = "cliente")
 public class Cliente {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id_cliente", nullable = false, updatable = false, unique = true)
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id_cliente", nullable = false, updatable = false)
     private Long id;
 
-    @Column(length = 80, nullable = false)
+    @Column(name = "nome", length = 80, nullable = false)
     private String nome;
 
-    @Column(length = 12, nullable = false)
-    private String rg;
+    @Column(name = "cpf", length = 15, nullable = false)
+    private String cpf;
+
+    @Column(name = "data_nascimento", nullable = false)
+    private LocalDate dataNascimento;
+
+    /**
+     * Pegar um objeto de referência e mapear para um objeto do tipo 'Cliente'
+     * @param function
+     * @param <R>
+     * @return
+     */
+    public <R> R map(Function<Cliente, R> function){
+        return function.apply(this);
+    }
 
     public Cliente(){
     }
 
-    public Cliente(Long id, String nome, String rg) {
+    public Cliente(Long id, String nome, String cpf, LocalDate dataNascimento) {
         this.id = id;
         this.nome = nome;
-        this.rg = rg;
+        this.cpf = cpf;
+        this.dataNascimento = dataNascimento;
     }
 
     public Long getId() {
@@ -42,11 +59,32 @@ public class Cliente {
         this.nome = nome;
     }
 
-    public String getRg() {
-        return rg;
+    public String getCpf() {
+        return cpf;
     }
 
-    public void setRg(String rg) {
-        this.rg = rg;
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public LocalDate getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(LocalDate dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Cliente cliente = (Cliente) o;
+        return Objects.equals(id, cliente.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
     }
 }
---
+++ src/main/java/br/com/emanuelgabriel/clienteapi/persistence/repository/ClienteRepository.java
@@ -0,0 +1,2 @@
+package br.com.emanuelgabriel.clienteapi.persistence.repository;public interface ClienteRepository {
+}
