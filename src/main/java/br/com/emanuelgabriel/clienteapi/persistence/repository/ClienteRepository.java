7f7e293cbba2996ba8c27effd8063414866690ab
Samuel Gabriel Sousa <emanuel.gabriel.sousa@protonmail.com>
2021-02-28 14:40:23 -0300

    criando o repository de cliente

--- src/main/java/br/com/emanuelgabriel/clienteapi/persistence/repository/ClienteRepository.java
+++ src/main/java/br/com/emanuelgabriel/clienteapi/persistence/repository/ClienteRepository.java
@@ -1,2 +1,11 @@
-package br.com.emanuelgabriel.clienteapi.persistence.repository;public interface ClienteRepository {
+package br.com.emanuelgabriel.clienteapi.persistence.repository;
+
+import br.com.emanuelgabriel.clienteapi.persistence.entity.Cliente;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ClienteRepository extends JpaRepository<Cliente, Long> {
+
+
 }
